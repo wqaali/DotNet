a7249dbfa71d24548ac20975999826ee582405b7
Waqar <55060717+wqaali@users.noreply.github.com>
2025-10-30 23:22:55 +0300

    Azure Semantic Kernal

---
+++ SemanticKernalDemo/SemanticKernalDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+namespace SemanticKernalDemo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Read Readme file how yo can create azure account and can get apiKey and other string values to run the prompt
+
+            var modelId = "";
+            var apiKey = "";
+            var endpoint = "";
+            var builder = Kernel.CreateBuilder();
+            builder.AddAzureOpenAIChatCompletion(modelId,endpoint,apiKey);
+            Kernel kernal=builder.Build();
+            var chatCompletionService = kernal.GetRequiredService<IChatCompletionService>();
+
+            while (true)
+            {
+                Console.WriteLine("\nEnter your prompt: ");
+                var prompt=Console.ReadLine();
+
+                if (string.IsNullOrEmpty(prompt))
+                    break;
+                var response = await chatCompletionService.GetChatMessageContentAsync(prompt);
+                Console.WriteLine(response);
+
+            }
+        }
+    }
+}
